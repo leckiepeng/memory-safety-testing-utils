7f0afdafb07a7e44dece4349ca49b52384ad48b3
Ott Tinn <llvm@otinn.com>
2012-07-13 10:48:42 +0100

    Make rtcc produce similar output to the LLVM pass.

--- rtccounter/RuntimeCheckCounter.cpp
+++ rtccounter/RuntimeCheckCounter.cpp
@@ -5,23 +5,41 @@
 namespace {
   long long LoadChecks, StoreChecks;
   long long FastLoadChecks, FastStoreChecks;
-  long long FastLoadCheckFailureCalls, FastStoreCheckFailureCalls;
+  long long FastLoadFailures, FastStoreFailures;
 
   void printLoadStoreSummary(FILE *F) {
-    long long Total = LoadChecks + StoreChecks + FastLoadChecks
-                    + FastStoreChecks + FastLoadCheckFailureCalls
-                    + FastStoreCheckFailureCalls;
+    long long GenericChecks = LoadChecks + StoreChecks;
+    long long FastChecks = FastLoadChecks + FastStoreChecks;
+    long long FastFailureCalls = FastLoadFailures + FastStoreFailures;
+    long long Sum = GenericChecks + FastChecks + FastFailureCalls;
 
-    fprintf(F, "====== Runtime Load/Store Check Counter Report ======\n");
-    fprintf(F, "  %lld load/store checks called in total\n", Total);
-    fprintf(F, "  %lld load checks called\n", LoadChecks);
-    fprintf(F, "  %lld store checks called\n", StoreChecks);
-    fprintf(F, "  %lld fast load checks called\n", FastLoadChecks);
-    fprintf(F, "  %lld fast store checks called\n", FastStoreChecks);
-    fprintf(F, "  %lld fast load check failures reported\n",
-            FastLoadCheckFailureCalls);
-    fprintf(F, "  %lld fast store check failures reported\n",
-            FastStoreCheckFailureCalls);
+    fprintf(F, "Runtime Load/Store Check Counter report: %lld checks called\n",
+            Sum);
+    if (!Sum)
+      return;
+
+    fprintf(F, "%lld generic load/store checks called (%d%%)\n",
+            GenericChecks, int(100 * GenericChecks / Sum));
+    if (GenericChecks) {
+      fprintf(F, "  %lld generic load checks\n", LoadChecks);
+      fprintf(F, "  %lld generic store checks\n", StoreChecks);
+    }
+
+    fprintf(F, "%lld fast load/store checks called (%d%%)\n",
+            FastChecks, int(100 * FastChecks / Sum));
+    if (FastChecks) {
+      fprintf(F, "  %lld fast load checks\n", FastLoadChecks);
+      fprintf(F, "  %lld fast store checks\n", FastStoreChecks);
+    }
+
+    fprintf(F, "%lld fast check failures reported (%d%%)\n",
+            FastFailureCalls, int(100 * FastChecks / Sum));
+    if (FastFailureCalls) {
+      fprintf(F, "  %lld fast load check failures reported\n",
+              FastLoadFailures);
+      fprintf(F, "  %lld fast store check failures reported\n",
+              FastStoreFailures);
+    }
   }
 
   void printReport() {
@@ -59,12 +77,12 @@ extern "C" {
   }
 
   void __fail_fastloadcheck(void *ptr, size_t size, void *obj, size_t obj_size) {
-    ++FastLoadCheckFailureCalls;
+    ++FastLoadFailures;
     exit(1);
   }
 
   void __fail_faststorecheck(void *ptr, size_t size, void *obj, size_t obj_size) {
-    ++FastStoreCheckFailureCalls;
+    ++FastStoreFailures;
     exit(1);
   }
 }
